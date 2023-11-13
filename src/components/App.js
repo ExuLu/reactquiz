d8f82fe1e2d75b6a8a11c7d0aec1524b35f4e332
Alena Sharai <exultantislupus@gmail.com>
2023-11-13 18:38:37 +0400

    refactor: change reducer function

--- src/components/App.js
+++ src/components/App.js
@@ -47,7 +47,12 @@ function reducer(state, action) {
           state.points > state.highscore ? state.points : state.highscore,
       };
     case 'restart':
-      return { ...state, status: 'ready', index: 0, answer: null, points: 0 };
+      return {
+        ...initialState,
+        status: 'ready',
+        questions: state.questions,
+        highscore: state.highscore,
+      };
     default:
       throw new Error('Action Unknown');
   }

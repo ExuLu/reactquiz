94819c95a22b11c476ae2e47db07be7d0a3cd871
Alena Sharai <exultantislupus@gmail.com>
2023-11-13 18:31:37 +0400

    feat: create RestartButton component which restart application

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = '🥇';
@@ -8,10 +8,13 @@ function FinishScreen({ points, maxPossiblePoints }) {
   if (percentage === 0) emoji = '🙈';
 
   return (
-    <p className='result'>
-      <span>{emoji}</span> You scored <strong>{points}</strong> out of{' '}
-      {maxPossiblePoints} ({Math.ceil(percentage)}%)
-    </p>
+    <>
+      <p className='result'>
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{' '}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className='highscore'>(Highscore: {highscore} points)</p>
+    </>
   );
 }
 

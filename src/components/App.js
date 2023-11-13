bed3d4fedabeec2f84dfe765d690a6912cea23cb
Alena Sharai <exultantislupus@gmail.com>
2023-11-13 17:55:15 +0400

    feat: add emoji accordinly to score

--- src/components/App.js
+++ src/components/App.js
@@ -37,6 +37,8 @@ function reducer(state, action) {
       };
     case 'nextQuestion':
       return { ...state, index: state.index + 1, answer: null };
+    case 'finish':
+      return { ...state, status: 'finished' };
     default:
       throw new Error('Action Unknown');
   }
@@ -82,7 +84,12 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
         )}
 

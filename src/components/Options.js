6fa1018f538bb214b4dca674b0f0dc1869def129
Alena Sharai <exultantislupus@gmail.com>
2023-11-13 16:08:37 +0400

    feat: add answer state, check if answer correct or not

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,22 @@
-function Options({ questionObj }) {
+function Options({ questionObj, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className='options'>
       {questionObj.options.map((option, i) => (
-        <button className='btn btn-option' key={option}>
-          {option} 
+        <button
+          className={`btn btn-option ${answer === i ? 'answer' : ''} ${
+            hasAnswered
+              ? i === questionObj.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          key={option}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: 'newAnswer', payload: i })}
+        >
+          {option}
         </button>
       ))}
     </div>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,10 @@
 import Options from './Options';
 
-function Question({ question }) {
-  console.log(question);
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options questionObj={question} />
+      <Options questionObj={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }

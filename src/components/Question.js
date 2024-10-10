761667b26c8848cd6391ac190a0cbd7ebdafd428
Alena Sharai <exultantislupus@gmail.com>
2024-10-10 21:25:28 +0400

    refactor: add context to question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,15 @@
 import Options from './Options';
 
-function Question({ question, dispatch, answer }) {
+import { useQuestions } from '../hooks/useQuestions';
+
+const Question = () => {
+  const { question, dispatch, answer } = useQuestions();
   return (
     <div>
       <h4>{question.question}</h4>
       <Options questionObj={question} dispatch={dispatch} answer={answer} />
     </div>
   );
-}
+};
 
 export default Question;
